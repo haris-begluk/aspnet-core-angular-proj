dbc0fceddbdf2a072e43e9d16fd387c5053956c6
haris-begluk <harisbegluk@gmail.com>
2018-10-02 16:36:45 +0200

    Configuring EF core, creating database and database context

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,14 @@
+using AspNetCoreAngularApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreAngularApp.Persistence
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+          
+        } 
+          public DbSet<Make> Makes { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,10 @@
+using AspNetCoreAngularApp.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -19,7 +21,8 @@ namespace AspNetCoreAngularApp
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        { 
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the Angular files will be served from this directory
