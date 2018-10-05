0cb15325f1dba27c2c113f0b75ca8865f7604209
haris-begluk <harisbegluk@gmail.com>
2018-10-05 13:36:40 +0200

    Building an api to update Objects correction

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -20,9 +20,9 @@ namespace AspNetCoreAngularApp.Mapping
             CreateMap<VehicleResource,Vehicle>() 
             .ForMember(v => v.Id, opt => opt.Ignore())
             .ForMember(v => v.ContactName, opt =>opt.MapFrom(vr => vr.Contact.Name))
-             .ForMember(v => v.ContactEmail, opt =>opt.MapFrom(vr => vr.Contact.Email))
-              .ForMember(v => v.ContactPhone, opt =>opt.MapFrom(vr => vr.Contact.Phone))
-               .ForMember(v => v.Features, opt =>opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature{FeatureId = id}))); 
+            .ForMember(v => v.ContactEmail, opt =>opt.MapFrom(vr => vr.Contact.Email))
+            .ForMember(v => v.ContactPhone, opt =>opt.MapFrom(vr => vr.Contact.Phone))
+            .ForMember(v => v.Features, opt =>opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature{FeatureId = id}))); 
 
         }
     }
