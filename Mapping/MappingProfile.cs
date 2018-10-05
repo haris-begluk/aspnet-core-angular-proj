b40dce9f96c9de5e7adc0da5128c0e4eedfd613f
haris-begluk <harisbegluk@gmail.com>
2018-10-05 14:04:17 +0200

    Refactoring using LINQ correction

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -27,13 +27,19 @@ namespace AspNetCoreAngularApp.Mapping
             .ForMember(v => v.Features, opt =>opt.Ignore()) 
             .AfterMap((vr, v) =>{
             //Remove unselected features   
-              var removedFeatures =  v.Features.Where(f => !vr.Features.Contains(f.FeatureId)).ToList();
+              var removedFeatures =  v.Features
+              .Where(f => !vr.Features
+              .Contains(f.FeatureId))
+              .ToList();
                    foreach (var rf in removedFeatures)
                         v.Features.Remove(rf);
             //Add new Features 
-              var addedFeatures =  vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)); 
-              foreach (var id in addedFeatures)
-                 v.Features.Add(new VehicleFeature{FeatureId = id});
+              var addedFeatures =  vr.Features
+              .Where(id => !v.Features.Any(f => f.FeatureId == id))
+              .Select(id => new VehicleFeature{FeatureId = id}) 
+              .ToList(); 
+              foreach (var f in addedFeatures)
+                 v.Features.Add(f);
             });
 
         }
