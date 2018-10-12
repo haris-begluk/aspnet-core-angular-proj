024b820ad30ed19d1ba0923759f8282fac5308f4
haris-begluk <harisbegluk@gmail.com>
2018-10-12 22:47:41 +0200

    Logging errors with sentry in production mode (sentry.io)

--- ClientApp/src/app/app-error-handler.ts
+++ ClientApp/src/app/app-error-handler.ts
@@ -1,13 +1,18 @@
 import * as Sentry from "@sentry/browser";
 import { ToastrService } from 'ngx-toastr';
-import {ErrorHandler, Inject, NgZone} from '@angular/core'; 
+import { ErrorHandler, Inject, NgZone, isDevMode } from '@angular/core'; 
 export class AppErrorHandler implements ErrorHandler { 
     constructor(private ngZone: NgZone,
         @Inject(ToastrService) private toastr:ToastrService){
 
     }
-    handleError(error: any):void{
-        Sentry.captureException(error.originalError || error);
+    handleError(error: any):void{  
+        //To test this if-else statment set environment variable to production and run application once again 
+        //then the errors on our web site will be logged to sentry application 
+        if(!isDevMode())
+        Sentry.captureException(error.originalError || error); 
+        else  
+        throw error;
         this.ngZone.run(() => {
             console.log('ERROR'); 
             this.toastr.error('An error happend!', "Error", {timeOut:5000});
--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/browser";
 import { AppErrorHandler } from './app-error-handler';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule, ErrorHandler} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
