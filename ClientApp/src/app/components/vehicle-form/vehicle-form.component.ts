e36d8310e3b4c6d421fce4dacb172fbf6acc8496
haris-begluk <harisbegluk@gmail.com>
2018-10-13 00:00:01 +0200

    Implementing editing, sending parallel requests and creating interfaces

--- ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,7 +1,12 @@
+import * as _ from 'underscore';
+import { Vehicle } from './../../models/vehicle';
+import { SaveVehicle } from "./../../models/SaveVehicle";
 import { ToastrService } from 'ngx-toastr';
 import { VehicleService } from '../../services/vehicle.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs'; 
+import 'rxjs/add/Observable/forkJoin';
 
 @Component({
   selector: 'app-vehicle-form',
@@ -12,9 +17,17 @@ export class VehicleFormComponent implements OnInit {
   makes: any[];  
   models: any[]; 
   features: any[];
-  vehicle: any ={ 
+  vehicle: SaveVehicle ={ 
+    id:0, 
+    makeId:0, 
+    modelId:0, 
+    isRegistered:false, 
     features:[], 
-    contact:{}
+    contact: {
+      name:'', 
+      email:'', 
+      phone:''
+    }
   };
  
   constructor( 
@@ -28,19 +41,31 @@ export class VehicleFormComponent implements OnInit {
     });
   }
 
-  ngOnInit() {  
-    this.vehicleService.getVehicle(this.vehicle.id) 
-    .subscribe( v => { 
-      this.vehicle = v;
+  ngOnInit() {    
+    var sources = [ 
+      this.vehicleService.getMakes(),
+      this.vehicleService.getFeatures()
+    ]; 
+    if(this.vehicle.id) 
+    sources.push(this.vehicleService.getVehicle(this.vehicle.id));
+    Observable.forkJoin(sources).subscribe( data => {
+      this.makes = data[0]; 
+      this.features = data[1];  
+      if(this.vehicle.id)
+      this.setVehicle(data[2]);
     }, err => { 
       if(err.status == 404) 
       this.router.navigate(['/home']);
     });
-    this.vehicleService.getMakes().subscribe(makes =>this.makes = makes
-    );  
-    this.vehicleService.getFeatures().subscribe(features => this.features = features);
-    
-  } 
+  }  
+  private setVehicle(v: Vehicle){
+    this.vehicle.id = v.id; 
+    this.vehicle.makeId = v.make.id; 
+    this.vehicle.modelId = v.model.id; 
+    this.vehicle.isRegistered = v.isRegistered; 
+    this.vehicle.contact = v.contact; 
+    this.vehicle.features = _.pluck(v.features, 'id');
+  }
   onMakeChange(){
    var selectedMake = this.makes.find( m => m.id == this.vehicle.makeId) 
       this.models =selectedMake ? selectedMake.models: []; 
---
+++ ClientApp/src/app/models/SaveVehicle.ts
@@ -0,0 +1,9 @@
+import { Contact } from "./Contact";
+export interface SaveVehicle {
+    id: number;
+    modelId: number;
+    makeId: number;
+    isRegistered: boolean;
+    features: number[];
+    contact: Contact;
+}
