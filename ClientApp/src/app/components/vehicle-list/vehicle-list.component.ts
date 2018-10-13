0d5a8ab9367110340945acbdca7d642836a0902f
haris-begluk <harisbegluk@gmail.com>
2018-10-13 02:41:11 +0200

    Vehicle filter

--- ClientApp/src/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/src/app/components/vehicle-list/vehicle-list.component.ts
@@ -1,3 +1,4 @@
+import { KeyValuePair } from './../../models/KeyValuePair';
 import { VehicleService } from './../../services/vehicle.service';
 import { Component, OnInit } from '@angular/core';
 import { Vehicle } from '../../models/vehicle';
@@ -8,12 +9,26 @@ import { Vehicle } from '../../models/vehicle';
   styleUrls: ['./vehicle-list.component.css']
 })
 export class VehicleListComponent implements OnInit {
-vehicles: Vehicle[];
+  vehicles: Vehicle[];  
+  allVehicles: Vehicle[];
+  makes: KeyValuePair[]; 
+  filter: any = {};
+  
   constructor(private vehicleService: VehicleService) { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    this.vehicleService.getMakes() 
+    .subscribe(makes => this.makes = makes);
     this.vehicleService.getVehicles()
-    .subscribe(vehicles => this.vehicles = vehicles);
+    .subscribe(vehicles => this.vehicles =this.allVehicles = vehicles);
+  } 
+  onFilterChange(){
+    var vehicles = this.allVehicles; 
+    if(this.filter.makeId) 
+    vehicles = vehicles.filter(v => v.make.id== this.filter.makeId); 
+    if(this.filter.modelId)
+    vehicles = vehicles.filter(v => v.model.id == this.filter.modelId); 
+    this.vehicles = vehicles;
   }
 
 }
